5a4148aac7e08f69a6738e0e7247b05715c9d0e7
redmundnacario <redmund.nacario@gmail.com>
2021-03-03 04:29:44 +0800

    readme updated

--- places-app/routes/places.routes.js
+++ places-app/routes/places.routes.js
@@ -11,7 +11,8 @@ const DUMMY_PLACES = [
         coordinates: {
             lat: 14.5794,
             lon: 121.0359
-        }
+        },
+        user_id: "u1"
     },
     {
         id: 2,
@@ -20,7 +21,8 @@ const DUMMY_PLACES = [
         coordinates: {
             lat: 14.5764,
             lon: 121.0851
-        }
+        },
+        user_id: "u2"
     }
 ]
 
@@ -31,6 +33,18 @@ router.get('/:pid', (req, res, next) => {
     })
     
     console.log("GET "+req.originalUrl)
-    res.json({place: place})
+    res.json({place})
 })
+
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid
+    const place = DUMMY_PLACES.find(value => {
+        return value.user_id == userId
+    })
+    console.log("GET "+req.originalUrl)
+    res.json({place})
+})
+
+
 module.exports = router;
