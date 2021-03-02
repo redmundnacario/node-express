d98dcd27100ee1ae749c95ebed0d3fc74ac64863
redmundnacario <redmund.nacario@gmail.com>
2021-03-03 03:49:43 +0800

    first commit : routes added

---
+++ places-app/routes/places.routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const router = express.Router()
+
+
+const DUMMY_PLACES = [
+    {
+        id: 1,
+        title: "Mandaluyong City",
+        description: "tiger city",
+        coordinates: {
+            lat: 14.5794,
+            lon: 121.0359
+        }
+    },
+    {
+        id: 2,
+        title: "Pasig City",
+        description: "Mayor Vico's city",
+        coordinates: {
+            lat: 14.5764,
+            lon: 121.0851
+        }
+    }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid
+    const place = DUMMY_PLACES.find((value) => {
+        return value.id == placeId
+    })
+    
+    console.log("GET "+req.originalUrl)
+    res.json({place: place})
+})
+module.exports = router;
